dbf6c81c508072c3c0a96bc5f07da2b4f6831bcc
anna-gorska <aniajj9@gmail.com>
2023-05-03 14:41:53 +0200

    last changes

--- SpectatorApp/Assets/Scripts/ConnectScreenViewer1.cs
+++ SpectatorApp/Assets/Scripts/ConnectScreenViewer1.cs
@@ -1,29 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ConnectScreenViewer1 : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Button secondButton;
+    private bool buttonEnabled = false;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (SyncPoseServer.isSecondPhaseDone){
+        if (SyncPoseServer.isSecondPhaseDone)
+        {
             this.gameObject.SetActive(false);
         }
-        else{
+        else if (SyncPoseServer.isFirstPhaseDone)
+        {
             this.gameObject.SetActive(true);
+            if (!buttonEnabled)
+            {
+                Debug.Log("Enabling second button");
+                StartCoroutine(EnableSecondButton());
+                buttonEnabled = true;
+            }
         }
-        
     }
 
-    public void setIsSecondButtonClicked(){
+    public void setIsSecondButtonClicked()
+    {
         SyncPoseServer.isButtonClicked = true;
     }
-    
+    IEnumerator EnableSecondButton()
+    {
+        Debug.Log("Start EnableSecondButton");
+        yield return new WaitForSeconds(2);
+        Debug.Log("Enable second button");
+        secondButton.interactable = true;
+    }
 }
